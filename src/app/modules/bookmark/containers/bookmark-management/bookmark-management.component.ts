4548349ed945ff5a6a2d8abe222860c1c7ebc5aa
PVADung <dung.pham@joikid.com>
2022-11-15 15:18:24 +0700

    refactor: remove test code

--- src/app/modules/bookmark/containers/bookmark-management/bookmark-management.component.ts
+++ src/app/modules/bookmark/containers/bookmark-management/bookmark-management.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { DialogService } from '@ui/dialog/services/dialog.service';
-import { ExampleComponent } from '../../components/example/example.component';
 
 @Component({
   selector: 'app-bookmark-management',
@@ -9,12 +8,12 @@ import { ExampleComponent } from '../../components/example/example.component';
 })
 export class BookmarkManagementComponent {
   constructor(public dialog: DialogService) {
-    const ref = this.dialog.open(ExampleComponent, {
-      data: { message: 'I am a dynamic component inside of a dialog!' },
-    });
+    // const ref = this.dialog.open(ExampleComponent, {
+    //   data: { message: 'I am a dynamic component inside of a dialog!' },
+    // });
 
-    ref.afterClosed$.subscribe(result => {
-      console.log('Dialog closed', result);
-    });
+    // ref.afterClosed$.subscribe(result => {
+    //   console.log('Dialog closed', result);
+    // });
   }
 }
