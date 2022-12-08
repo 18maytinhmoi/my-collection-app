ec93126e494bc7797f8c989910af7e080f9170cc
PVADung <dung.pham@joikid.com>
2022-12-08 18:05:35 +0700

    dependencies: swagger

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './authentication/auth.module';
+import { appConfig } from './configs/app.config';
 import { authConfig } from './configs/auth.config';
 import { SecurityModule } from './modules/security/security.module';
 import { UserModule } from './modules/user/user.module';
@@ -11,7 +12,7 @@ import { UserModule } from './modules/user/user.module';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      load: [authConfig],
+      load: [authConfig, appConfig],
     }),
     AuthModule,
     UserModule,
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,38 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { appConfig, AppConfig } from './configs/app.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = app.get<AppConfig>(appConfig.KEY);
+
+  app.enableCors();
+  app.enableCors({ credentials: true, origin: config.client });
+  app.enableShutdownHooks();
+
+  const globalPrefix = 'api';
+  app.setGlobalPrefix(globalPrefix);
+
+  const swaggerOptions = new DocumentBuilder()
+    .setTitle('Chat App')
+    .setDescription('API Document')
+    .setVersion('1.0.0')
+    .addServer(config.domain, 'Development API')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerOptions);
+
+  SwaggerModule.setup(`${globalPrefix}/docs`, app, document, {
+    swaggerOptions: {
+      docExpansion: 'list',
+      filter: true,
+      showRequestDuration: true,
+    },
+  });
+
+  await app.listen(config.port);
+
+  console.log(`Server is listening on :${config.port}/${globalPrefix}`);
 }
 bootstrap();
